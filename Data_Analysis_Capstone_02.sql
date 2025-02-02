2223e611aa7e077e88ed9e2680ad2a754536ab52
Abianga Godwin <110383997+abianga-Godwin@users.noreply.github.com>
2025-02-01 21:24:26 -0700

    Update Data_Analysis_Capstone_02.sql

--- Data_Analysis_Capstone_02.sql
+++ Data_Analysis_Capstone_02.sql
@@ -6,32 +6,25 @@ DROP COLUMN string_field_18;
 SELECT 
   *
 FROM 
-  `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.202312_2024_Cyclistic_TripData`
+  `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.202312_2024_Cyclistic_TripData`; 
 
-*/
-
-/*
 
 -- Step 1: Create the new table with all data from the old table
+
 CREATE TABLE `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.2024_Cyclistic_TripData` AS
 SELECT *
 FROM `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.new_table`;
 
 -- Step 2: Drop the old table
+
 DROP TABLE `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.new_table`;
 
-*/
-
-/*
 
 DELETE FROM
   `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.2024_Cyclistic_TripData`
 WHERE
-  rideable_type IS NULL
+  rideable_type IS NULL;
 
-  */
-
-/*
 
 SELECT
   * 
@@ -39,20 +32,18 @@ FROM
   `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.2024_Cyclistic_TripData`
 ORDER BY
   started_at,
-  ended_at ASC
+  ended_at ASC;
 
-  */
+ 
 
-/*UPDATE
+UPDATE
   `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.TEST_2024_Cyclistic_TripData`
 SET
    ride_length = REPLACE(ride_length, '0:', '00:')
 WHERE
   ride_length IS NOT NULL;
 
-*/
 
-/*
 
 DROP TABLE `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.2024_Cyclistic_TripData`; ----- VERY IMPORTANT
 
@@ -60,8 +51,7 @@ CREATE TABLE `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.2024_Cyclisti
 SELECT
     *
 FROM
-    `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.TEST_2024_Cyclistic_TripData`
-*/
+    `constant-tracer-438121-p1.My_Cyclistic_FullData_2024.TEST_2024_Cyclistic_TripData`;
 
 
 
